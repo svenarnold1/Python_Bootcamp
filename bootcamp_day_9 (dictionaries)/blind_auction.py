6bf191f69009a7b72cc9c4ced36f0e2e045ff105
svenarnold1 <sven.arnold97@gmail.com>
2023-03-09 22:34:26 +0100

    solved blind auction problem, felt easy

--- bootcamp_day_9 (dictionaries)/blind_auction.py
+++ bootcamp_day_9 (dictionaries)/blind_auction.py
@@ -1,29 +1,32 @@
+from auction_logo import logo
 # this is a blind auction
 bids = {}
 keep_running = True
 score = 0
 winner = 'no one'
+
+print(logo)
 while keep_running:
 
     name = input('Input your name:\n')
-    bid = int(input('Input your bid:'))
+    bid = float(input('Input your bid: $'))
 
     # add name and price in dictionary
     bids[name] = bid
 
-    again = input('Are there other users who want to bid?\n').lower()
+    should_continue = input('Are there other users who want to bid?\n').lower()
 
-    if again == 'no':
+    if should_continue == 'no':
         keep_running = False
     else:
         print('\n'*500)  # clear the console
 
 # finding the winner.
 for name in bids:
-     if bids[name] > score:
-         score = bids[name]
-         winner = name
-     else:
-         continue
+    if bids[name] > score:
+        score = bids[name]
+        winner = name
+    else:
+        continue
 
-print(f"Congratulations! {winner.upper()} has won the auction with the bid of {bids[winner]}$!")
+print(f"Congratulations! {winner.upper()} has won the auction with the bid of ${bids[winner]}!")
