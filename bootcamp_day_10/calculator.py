c9590b80b719a589753a638bed3e314845394296
svenarnold1 <sven.arnold97@gmail.com>
2023-03-13 18:35:28 +0100

    finished day 10 at Python Bootcamp!

--- bootcamp_day_10/calculator.py
+++ bootcamp_day_10/calculator.py
@@ -1,53 +1,48 @@
-# This is a calculator
-# function to add two numbers
-def add(n1, n2):
-    return n1 + n2
-
-
-# function to subtract two numbers
-def subtract(n1,n2):
-    return n1 - n2
-
-
-# function to multiply two numbers
-def multiply(n1,n2):
-    return n1 * n2
-
-
-# function to divide two numbers
-def divide(n1, n2):
-    return n1 / n2
+import calculator_functions
 
 
 # operators available in calculator
 operations = {
-    '+': add,
-    '-': subtract,
-    '*': multiply,
-    '/': divide
+    '+': calculator_functions.add,
+    '-': calculator_functions.subtract,
+    '*': calculator_functions.multiply,
+    '/': calculator_functions.divide
 }
 
+
 # first input for the program
-num_1 = float(input('Input your first number: '))
-for key in operations:
-    print(key)
-operation_symbol = input('Pick an operator from the line above? ')
-num_2 = float(input('Input your second number: '))
+def calculator():
+    print(calculator_functions.logo)
+    num_1 = float(input('Input your first number: '))
+    for key in operations:
+        print(key)
+    operation_symbol = input('Pick an operator from the line above? ')
+    num_2 = float(input('Input your second number: '))
 
-# choose the function we need and store result inside of variable
-calculation_function = operations[operation_symbol]
-result = calculation_function(num_1, num_2)
+    # choose the function we need and store result inside of variable
+    calculation_function = operations[operation_symbol]
+    result = calculation_function(num_1, num_2)
 
-# print the result
-print(f"{num_1} {operation_symbol} {num_2} = {result}")
+    # print the result
+    print(f"{num_1} {operation_symbol} {num_2} = {result}")
+    keep_going = True
+    while keep_going:
+        move_on = input(f"Type 'y' if you want to continue calculation with {result}, otherwise type 'n'!\n").lower()
+        if move_on == 'y':
+            # why return in function instead of print? - so we can set function as argument for other function!
+            for key in operations:
+                print(key)
+            operation_symbol = input('Pick an operator from the line above? ')
+            num_3 = float(input('Input your next number: '))
+            calculation_function = operations[operation_symbol]
+            result_2 = calculation_function(result, num_3)
+            # print the result
+            print(f"{result} {operation_symbol} {num_3} = {result_2}")
+            result = result_2
+        else:
+            keep_going = False
+            calculator()
 
-# why return in function instead of print? - so we can set function as argument for other function!
-num_3 = float(input('Input your next number: '))
-for key in operations:
-    print(key)
-operation_symbol = input('Pick an operator from the line above? ')
-calculation_function = operations[operation_symbol]
-result_2 = calculation_function(result, num_3)
 
-# print the result
-print(f"{result} {operation_symbol} {num_3} = {result_2}")
+# call the calcutator function above
+calculator()
---
+++ bootcamp_day_10/calculator_functions.py
@@ -0,0 +1,37 @@
+# this is the logo of the calculator program
+logo = """
+ _____________________
+|  _________________  |
+| | Welcome!     0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|"""
+
+# function we need to calculate
+
+# function to add two numbers
+def add(n1, n2):
+    return n1 + n2
+
+
+# function to subtract two numbers
+def subtract(n1,n2):
+    return n1 - n2
+
+
+# function to multiply two numbers
+def multiply(n1,n2):
+    return n1 * n2
+
+
+# function to divide two numbers
+def divide(n1, n2):
+    return n1 / n2
