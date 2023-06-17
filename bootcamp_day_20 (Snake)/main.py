8f5f13e1e93fae4b8c22206fd8ae5e88851225dc
svenarnold1 <sven.arnold97@gmail.com>
2023-06-17 15:20:50 +0200

    Finished Snake.

--- bootcamp_day_20 (Snake)/main.py
+++ bootcamp_day_20 (Snake)/main.py
@@ -48,10 +48,8 @@ while is_game_on:
         score.game_over()
 
     # Detect collision with tail
-    for segment in snake.segments:
-        if segment == snake.segments[0]:
-            pass
-        elif snake.segments[0].distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.segments[0].distance(segment) < 10:
             is_game_on = False
             score.game_over()
 
