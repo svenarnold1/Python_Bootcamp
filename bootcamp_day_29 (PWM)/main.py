f2bd8b3a269802e81c5614f6202a52f20c4ed632
svenarnold1 <sven.arnold97@gmail.com>
2024-03-19 21:37:33 +0100

    started to build a password manager.

--- bootcamp_day_29 (PWM)/main.py
+++ bootcamp_day_29 (PWM)/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 FONT_NAME = "Arial"
+FONT = (FONT_NAME, 10, "normal")
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -8,36 +9,41 @@ FONT_NAME = "Arial"
 # setup window
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20, bg="#FFFFFF")
+window.config(padx=50, pady=50, bg="#FFFFFF")
 
 
 # setup canvas
 canvas = Canvas(width=200, height=200, bg='white', highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 112, image=logo_img)
+canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
 # Label's
-url_label = Label(text="Website:", bg="white", font=(FONT_NAME, 8, "bold"))
+url_label = Label(text="Website:", bg="white", font=FONT)
 url_label.grid(column=0, row=1)
-username_label = Label(text="Email/Username:", bg="white", font=(FONT_NAME, 8, "bold"))
+username_label = Label(text="Email/Username:", bg="white", font=FONT)
 username_label.grid(column=0, row=2)
-password_label = Label(text="Password:", bg="white", font=(FONT_NAME, 8, "bold"))
+password_label = Label(text="Password:", bg="white", font=FONT)
 password_label.grid(column=0, row=3)
 
-# # Entries
-# url_entry = Entry(width=35, columnspan=2)
-# # Add some text to begin with
-# url_entry.insert(END, string="www.example.com")
-# url_entry.pack(column=1, row=1)
+# Entries
+url_entry = Entry(width=35)
+url_entry.insert(END, string="www.example.com")
+url_entry.grid(column=1, row=1, columnspan=2)
+
+user_entry = Entry(width=35)
+user_entry.insert(END, string="user.pattern@gmail.com")
+user_entry.grid(column=1, row=2, columnspan=2)
+
+pw_entry = Entry(width=21)
+pw_entry.grid(column=1, row=3)
 
 # setup buttons
-generate_button = Button(text="Generate Password", bg="white", fg="black", highlightthickness=0)
-generate_button.grid(column=3, row=3)
+generate_button = Button(text="Generate Password", bg="white")
+generate_button.grid(column=2, row=3)
 
-
-add_button = Button(text="Add", bg="white", fg="black", highlightthickness=0)
-add_button.grid(column=2, row=4)
+add_button = Button(text="Add", bg="white", width=30)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
 
